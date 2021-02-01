e44e86858c63430662bb541b6cda3bbea03e1df3
CalDough <77810418+CalDough@users.noreply.github.com>
2021-01-31 19:39:05 -0500

    Finished the exercises
    Completed the main assignment as well as attempted the one for extra credit.

--- main2.py
+++ main2.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 import sys
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+
+import random
+number = random.randrange(1,20)
+guess = input("I am thinking of a number between 1 - 20. What number do you think it is? ")
+guess = int(guess)
+if guess == number:
+    print("You were actually able to guess it correctly??? Fine... I bet you can't do it again.")
+else:
+     print("Sorry, but you are incorrect. Better luck next time.")
+     print("The number I was thinking of was " + str(number))
